31dffe96a83fe24bdd97734d899156fe8a17d354
Chris Allen <gh.84kjuqg365@gmail.com>
2023-08-13 13:52:26 -0700

    Get input from form, save to var.

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    ;
+  }
+
+  onSubmit(formData: NgForm){
+    let categoryData = {
+      category: formData.value.category
+    }
+
+    console.log(categoryData);
+
+  }
 
 }
