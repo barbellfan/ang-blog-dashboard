3fa43a3d6798d5daed50d3fcaee9ec52d628422d
Chris Allen <gh.84kjuqg365@gmail.com>
2023-09-23 13:55:04 -0700

    Automatically set permalink Other minor tweaks to css.

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
+
+  permalink: string = '';
+
+  ngOnInit(): void {
+  }
+
+  onTitleChanged($event: any) {
+    const title: string = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
+
+  }
 
 }
