02e78774316f0c0c1f100a39dba72e78e775afe8
Chris Allen <gh.84kjuqg365@gmail.com>
2023-09-24 14:39:18 -0700

    Set values in category drop down

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FsCategory } from 'src/app/models/fs-category';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-new-post',
@@ -10,8 +12,21 @@ export class NewPostComponent implements OnInit {
   permalink: string = '';
   imgSrc: any = './assets/placeholder-image.jpg';
   selectedImage: any;
+  categories: Array<FsCategory> = [];
+
+  constructor(private categoryService: CategoriesService) { }
 
   ngOnInit(): void {
+    let thisthis = this;
+    this.categoryService.loadData().then(
+      function(val: FsCategory[]) {
+        thisthis.categories = val;
+      },
+      function(error) {
+        console.log("data not loaded: " + error);
+
+      }
+    );
   }
 
   onTitleChanged($event: any) {
