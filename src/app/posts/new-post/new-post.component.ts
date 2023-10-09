17f16c21809ed8d168673334a13c49d6f05709d0
Chris Allen <gh.84kjuqg365@gmail.com>
2023-10-09 14:50:54 -0700

    Add post interface. Set it in onSubmit().

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { FsCategory } from 'src/app/models/fs-category';
+import { Post } from 'src/app/models/post';
 import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
@@ -63,6 +64,21 @@ export class NewPostComponent implements OnInit {
 
   onSubmit() {
     console.log(this.postForm.value);
+    const postData: Post = {
+      title: this.postForm.value.title,
+      permalink: this.postForm.value.permalink,
+      category: {
+        categoryId: '',
+        category: ''
+      },
+      postImgPath: '',
+      excerpt: this.postForm.value.excerpt,
+      content: this.postForm.value.content,
+      isFeatured: false,
+      views: 0,
+      status: 'new',
+      createdAt: new Date()
+    };
   }
 
 }
