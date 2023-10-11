616795d1ecf6920cc5311e77aba8d96113d75f8b
Chris Allen <gh.84kjuqg365@gmail.com>
2023-10-11 13:40:00 -0700

    Upload image to file storage Had to look up how to do this. Video is out of date.

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
-import { FirebaseStorage, StorageModule } from '@angular/fire/storage';
+import { Injectable, inject } from '@angular/core';
+import { Storage,ref, uploadBytesResumable } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  private storage: Storage = inject(Storage);
 
-  constructor(private storage: StorageModule) { }
+  constructor() { }
 
   uploadImage(selectedImage: any) {
     const filePath = `postIMG/${Date.now()}`;
     console.log(filePath);
 
+    // From example at:
+    //https://github.com/angular/angularfire/blob/master/docs/storage.md
+    const storageRef = ref(this.storage, filePath);
+    uploadBytesResumable(storageRef, selectedImage).then(() => {
+      console.log("post image uploaded successfully");
+    });
   }
 }
