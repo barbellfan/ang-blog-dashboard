496dd98bfe72b74d9b79ac4d29dd8e4efd164b34
Chris Allen <gh.84kjuqg365@gmail.com>
2023-11-25 14:27:21 -0800

    navigate to root on login success on error, show a better toaster warning

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Auth, getAuth, signInWithEmailAndPassword } from '@angular/fire/auth'
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
@@ -7,14 +8,15 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AuthService {
 
-  constructor(private auth: Auth, private toaster: ToastrService) { }
+  constructor(private auth: Auth, private toaster: ToastrService, private router: Router) { }
 
   login(email: string, password: string) {
 
     signInWithEmailAndPassword(getAuth(), email, password).then(logRef => {
       this.toaster.success('Logged in successfully');
+      this.router.navigate(['/']);
     }).catch(error => {
-      this.toaster.error(`Error logging in: ${error}`);
+      this.toaster.warning(error.message);
     });
   }
 }
